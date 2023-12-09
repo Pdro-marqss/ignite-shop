bda5cdb515e8a8fc58b51faa3254e99cbefd043c
Pedro Marques <pdro.marqs2015@hotmail.com>
2023-12-09 14:50:03 -0300

    feat: product page structure and style

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+
+import { ProductContainer, ImageContainer, ProductDetails } from '../../styles/pagesStyles/product';
 
 export default function Product() {
    const { query } = useRouter();
 
    return (
-      <h1>ProductPage</h1>
+      <ProductContainer>
+         <ImageContainer>
+
+         </ImageContainer>
+
+         <ProductDetails>
+            <h1>Camiseta X</h1>
+            <span>R$ 79,99</span>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, iure temporibus fugiat voluptatum quidem quos debitis ad labore odio! Illum illo commodi quae quasi odit, officiis rerum reiciendis inventore libero.</p>
+
+            <button>Comprar agora</button>
+         </ProductDetails>
+      </ProductContainer>
    )
 }
