7d6acd6e0d4f0665b98dbd4186efb4a5883dcb1b
Pedro Marques <pdro.marqs2015@hotmail.com>
2023-12-12 02:56:16 -0300

    feat: success page structure and style

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pagesStyles/success";
+
 export default function Success() {
    return (
-      <h1>Success page</h1>
+      <SuccessContainer>
+         <h1>Compra efetuada</h1>
+
+         <ImageContainer>
+
+         </ImageContainer>
+
+         <p>
+            Uhuul <strong>Pedro Marques</strong>, sua Camiseta <strong>Ignite Aboard</strong> já está a caminho da sua casa.
+         </p>
+
+         <Link href="/">
+            Voltar ao catálogo
+         </Link>
+      </SuccessContainer>
    )
 }
